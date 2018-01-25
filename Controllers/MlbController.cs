0d7e4f91179a422acb443354827d36f1cf5be601
tsubakimoto <tkmbmbr@gmail.com>
2018-01-25 13:50:09 +0900

    MlbControllerの作成

---
+++ Controllers/MlbController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myvueasp.Models;
+using Newtonsoft.Json;
+
+namespace myvueasp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class MlbController : Controller
+    {
+        private IEnumerable<Team> _teams = null;
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Team()
+        {
+            if (_teams == null || !_teams.Any())
+            {
+                var response = await MakeRequest();
+                var content = await response.Content.ReadAsStringAsync();
+                _teams = JsonConvert.DeserializeObject<IEnumerable<Team>>(content);
+            }
+            return Ok(_teams);
+        }
+
+        private static async Task<HttpResponseMessage> MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "1ae6fa2ee513451cb5992ff9c91b5a3d");
+
+            var uri = "https://api.fantasydata.net/v3/mlb/scores/JSON/teams?" + queryString;
+
+            return await client.GetAsync(uri);
+        }
+
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,21 @@
+﻿namespace myvueasp.Models
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+        public string Key { get; set; }
+        public bool Active { get; set; }
+        public string City { get; set; }
+        public string Name { get; set; }
+        public int StadiumID { get; set; }
+        public string League { get; set; }
+        public string Division { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public string TertiaryColor { get; set; }
+        public string QuaternaryColor { get; set; }
+        public string WikipediaLogoUrl { get; set; }
+        public string WikipediaWordMarkUrl { get; set; }
+        public int GlobalTeamID { get; set; }
+    }
+}
